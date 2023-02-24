5a26bd0ea242c9b190289b467b54297ca0cc3fb8
wioenena- <wioenena@gmail.com>
2023-02-25 00:01:17 +0300

    test: successfully tested

---
+++ test.ts
@@ -0,0 +1,23 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { Cooldown } from "./main.ts";
+
+const cooldown = new Cooldown(1000 * 10, 5);
+const ID = "test";
+
+Deno.test({
+  name: "On cooldown",
+  sanitizeResources: false,
+  sanitizeOps: false,
+  fn() {
+    assertEquals(cooldown.cache.size, 0);
+    for (let i = 0; i < 5; i++) {
+      assertEquals(cooldown.onCooldown(ID), false);
+    }
+    assertEquals(cooldown.cache.size, 1);
+    assertEquals(cooldown.onCooldown(ID), true);
+    setTimeout(() => {
+      assertEquals(cooldown.cache.size, 0);
+      assertEquals(cooldown.onCooldown(ID), false);
+    }, 1000 * 11);
+  },
+});
